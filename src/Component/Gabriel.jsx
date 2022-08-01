0848daa53c2980add9842bffdd84b5966f3dfcdd
Gabriel M. Severio <gg.4004116@gmail.com>
2022-07-31 22:44:14 -0300

    src

---
+++ src/Component/Gabriel.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import './Style.css'
+
+export default function Gabriel() {
+  return (
+    <div>
+      <h1>Hello World</h1>
+      <p>
+        Welcome to my site in React JS
+      </p>
+      <p>
+        
+      </p>
+
+      <div class="t">
+
+      </div>
+    </div>
+  )
+}
